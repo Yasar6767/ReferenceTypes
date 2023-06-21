75f182637ea980dd08fe38121c9973676f9b74b2
Yaşar Mehmet Akat <akat434@gmail.com>
2023-06-21 12:35:43 +0300

    Add project files.

---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int,decimal,enum,float,boolean value types
+            //int sayi1 = 10;
+            //int sayi2 = 20;
+            //sayi1 = sayi2;
+            //sayi2 = 100;
+
+            //Console.WriteLine("Sayı 1 : " + sayi1);
+
+            //array,class,inteface ... reference types
+            //int[] sayilar1 = new int[] { 1, 2, 3 };
+
+            //int[] sayilar2 = new int[] { 100, 200, 300 };
+
+            //sayilar1 = sayilar2;
+            //sayilar2[0] = 1000;
+            //Console.WriteLine("Sayilar1[0] : " + sayilar1[0]);
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.FirstName = "Yaşar";
+            person2 = person1;
+            person1.FirstName = "Elif";
+            //Console.WriteLine(person2.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Azra";
+            customer.CreditCardNumber = "1234567890";
+            Employee employee = new Employee();
+            employee.FirstName = "Veli";
+
+            Person person3 = customer;
+
+            //Console.WriteLine(person3.FirstName);
+            //Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            PersonelManager personelManager = new PersonelManager();
+            personelManager.Add(person3);
+            personelManager.Add(customer);
+            personelManager.Add(person2);
+            personelManager.Add(person1);
+            personelManager.Add(employee);
+
+        }
+    }
+
+    //base class : Person 
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+
+    class PersonelManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
---
+++ ReferenceTypes2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ReferenceTypes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Magaza magaza1 = new Magaza();
+            Magaza magaza2 = new Magaza();
+
+            magaza1.Name = "Boyner";
+            magaza1 = magaza2;
+
+            Gıyım gıyım = new Gıyım();
+            gıyım.Mont = "DeriMont";
+            gıyım.Pantolon = "Mavi";
+            Ayakkabi ayakkabi = new Ayakkabi();
+            ayakkabi.Spor = "Adidas";
+            ayakkabi.Name = "Akatlar";
+            Kozmetik  kozmetik = new Kozmetik();
+            kozmetik.Ruj = "Avon Mat Ruj";
+            kozmetik.Krem = "Nıvea";
+
+            Magaza magaza3 = kozmetik;
+            magaza3.Name = "JackJones";
+
+
+            Console.WriteLine(((Kozmetik)magaza3).Krem);
+
+            MagazaManager magazaManager = new MagazaManager();
+
+            magazaManager.Add(kozmetik);
+            magazaManager.Add(ayakkabi);
+        }
+
+    }
+    class Magaza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Amblem { get; set; }
+    }
+    class Gıyım:Magaza
+    {
+        public string Pantolon { get; set; }
+        public string TShirt {get; set; }
+        public string Gomlek { get; set; }
+        public string Mont { get; set; }
+
+
+    }
+
+    class Ayakkabi:Magaza
+    {
+        public string Spor { get; set; }
+        public string Klasik { get; set; }
+        public string Ortepedik { get; set; }
+    }
+
+    class Kozmetik:Magaza
+    {
+        public string Krem { get; set; }
+        public string Ruj { get; set; }
+        public string Deodorant { get; set; }
+    }
+
+    class MagazaManager 
+    {
+        public void Add(Magaza magaza) 
+        {
+            Console.WriteLine(magaza.Name);
+        }
+    }
+}
